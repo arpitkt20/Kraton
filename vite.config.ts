60f2f22b932a20f6478aec54466485a98db14084
arpitkt20 <arpitktiwari@gmail.com>
2026-01-05 02:48:24 +0530

    dist changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES === 'true' ? '/Kraton/' : '/',
+  base: '/Kraton/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
